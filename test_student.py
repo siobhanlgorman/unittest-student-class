292b4e952982dfd3ff42327fc1a1d501269795a4
siobhanlgorman <siobhanlgorman@gmail.com>
2021-09-15 08:19:23 +0000

    Add setUp and tearDown class methods to run once at beginning and end of tests and setUp and tearDown methods to run before and after each test methodand used setUp method to automatically create instance of student and refactor code

--- test_student.py
+++ test_student.py
@@ -4,20 +4,33 @@ from student import Student
 
 class TestStudent(unittest.TestCase):
 
-    def test_full_name(self):
-        student = Student('John', 'Doe')
+    @classmethod
+    def setUpClass(cls):
+        print('setUpClass')
 
-        self.assertEqual(student.full_name, 'John Doe')
+    @classmethod
+    def tearDownClass(cls):
+        print('tearDownClass')
+
+    def setUp(self):
+        print('setUp')
+        self.student = Student('John', 'Doe')
+
+    def tearDown(self):
+        print('tearDown')
+
+    def test_full_name(self):
+        print('test_full_name')
+        self.assertEqual(self.student.full_name, 'John Doe')
 
     def test_alert_santa(self):
-        student = Student('John', 'Doe')
-        student.alert_santa()
-
-        self.assertTrue(student.naughty_list)
+        print('test_alert_santa')
+        self.student.alert_santa()
+        self.assertTrue(self.student.naughty_list)
 
     def test_email(self):
-        student = Student('John', 'Doe')
-        self.assertEqual(student.email, "john.doe@email.com")
+        print('test_email')
+        self.assertEqual(self.student.email, "john.doe@email.com")
 
 
 if __name__ == "__main__":

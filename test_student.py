3217ccb0328cd9a3d6887712850e6c06a343b9a8
siobhanlgorman <siobhanlgorman@gmail.com>
2021-09-16 08:18:10 +0000

    Add passing test for successful request for student's course schedule from fictional API

--- test_student.py
+++ test_student.py
@@ -1,6 +1,7 @@
 import unittest
 from student import Student
 from datetime import timedelta
+from unittest.mock import patch
 
 
 class TestStudent(unittest.TestCase):
@@ -47,6 +48,14 @@ class TestStudent(unittest.TestCase):
         old_end_date = self.student.end_date
         self.student.apply_extension(5)
         self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5))
+
+    def test_course_schedule_success(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = "Success"
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Success")
 
 
 if __name__ == "__main__":
